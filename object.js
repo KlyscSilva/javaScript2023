f58efffed04ffcce47459bee4aea8d5442bf5e0b
KlyscSilva <smatheussilva@hotmail.com>
2023-02-28 21:44:19 -0300

    alterando

---
+++ object.js
@@ -0,0 +1,39 @@
+/*
+    Objeto 
+
+    Contêm propriedade e valor:
+
+    marca : samsung
+    cor : preta
+    tamanho: "50"
+
+    nome : Matheus
+    idade: 26
+    altura: 1.7m
+
+
+    sempre usar virgula de uma propriedade pra outra ao final
+
+*/
+
+// Criando um objeto
+
+
+const person = {
+
+    name: "Matheus",
+    age: 26,
+    sex: "M",
+    height: 1.73,
+    adress:{
+
+        street: "shishau",
+        number: 57,
+        country: "Brasil"
+
+    }
+
+}
+
+console.log(person.name);
+console.log(person.adress.country);
