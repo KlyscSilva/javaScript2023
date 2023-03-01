345325d5f939078f77bb22bc6f73fdd5be961b09
KlyscSilva <smatheussilva@hotmail.com>
2023-02-28 21:57:59 -0300

    valores boolean

---
+++ boolean.js
@@ -0,0 +1,19 @@
+/*
+    Boolean
+
+    valores que só têm duas opções 0 ou 1
+    no javascript dizemos true or false
+
+    = sinal para atribuir valor
+    == sinal para comparar valor
+*/
+
+
+let myNumberOne = 10
+let myNumberTwo = 20
+
+const result = myNumberOne == myNumberTwo
+
+console.log(result);
+
+
