ee02abd59b758396f3cfd38330c629ed5214c414
KlyscSilva <smatheussilva@hotmail.com>
2023-02-28 22:51:13 -0300

    up

---
+++ raizQuadrada.js
@@ -0,0 +1,10 @@
+/*
+
+ Faça um programa que encontre a raiz quadrada de um número, multiplique o resultado por 10 e divida por 33
+
+*/
+
+let number = 25
+const result = Math.sqrt(number) * 10 / 33
+
+console.log(result);
---
+++ stringDesafio.js
@@ -0,0 +1,14 @@
+/*
+
+Faça um programa que inicie com dois nomes, e o programa imprima na tela o seguinte dado: Olá, meu nome é (nome 1) e meu partner do CodeClub é (nome2)
+
+*/
+
+
+let nameOne = "Matheus"
+let nameTwo = "Wilson"
+
+let myTextNew = `Olá, meu nome é ${nameOne} e meu partner da mentoria é ${nameTwo}.`
+
+console.log(myTextNew);
+
