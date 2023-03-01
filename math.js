4e5cdf6a248f254912b41900e5f723d71b5fcf1e
KlyscSilva <smatheussilva@hotmail.com>
2023-02-28 22:25:27 -0300

    alguns Math

---
+++ math.js
@@ -0,0 +1,13 @@
+/*
+    Math - Ferramenta do JS
+
+    OBS: todas ferramentas abaixo são uma função
+
+*/
+
+console.log(Math.PI); // Mostra o valor do PI
+console.log(Math.pow(2,5)); // Permite usarmos potenciação - primeiro numero a base e segundo expoente
+console.log(Math.sqrt(25)); // Calcular a raiz de um número
+console.log(Math.random()); // Mostra número aleatorio entre 0 e 1
+console.log(Math.ceil(23.6)); // Arredonda um número para cima
+console.log(Math.floor(23.6)); // Arredonda um número pra baixo
