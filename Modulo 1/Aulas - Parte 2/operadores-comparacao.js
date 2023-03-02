4264293a749c405eb13ec4716d54b9ab9828c5be
KlyscSilva <smatheussilva@hotmail.com>
2023-03-02 00:21:43 -0300

    UP

---
+++ Modulo 1/Aulas - Parte 2/operadores-comparacao.js
@@ -0,0 +1,10 @@
+/*
+    Operadores de Comparação
+
+    == -> Igual, ele compara o valor,mas não compara o tipo do valor
+    === -> TOTALMENTE igual, ele verifica o tipo do dado! -> SEMPRE USAR ESSE OPERADOR DE COMPARAÇAO
+
+    !=  -> Diferente,ele compara o valor,mas não compara o tipo do valor
+    !== -> TOTALMENTE diferente, ele verifica o tipo do dado!  -> SEMPRE USAR ESSE OPERADOR DE COMPARAÇAO
+
+*/
