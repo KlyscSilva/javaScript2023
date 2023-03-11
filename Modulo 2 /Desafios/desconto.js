e4bd22a06a04bc3d8bbee9433711b69de5e91cbd
KlyscSilva <smatheussilva@hotmail.com>
2023-03-11 15:00:00 -0300

    up

--- Modulo 2 /Desafios/desconto.js
+++ Modulo 2 /Desafios/desconto.js
@@ -328,10 +328,6 @@ return
     /*
     
 
-    
-    Se o cliente recebeu desconto, imprima na tela um agradecimento pela compra, o valor total sem desconto, o valor total com desconto e a porcentagem de desconto que ele recebeu.
-    Se o cliente não recebeu desconto, imprima na tela um agradecimento pela compra, o valor total da sua compra e um cupom de desconto:
-    Esse cupom deve ser um valor aleatório entre 10 e 20% para a próxima compra. CRIAR UM RAMDOM
 
 
 
